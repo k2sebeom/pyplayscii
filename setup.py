206882597b77e08b5729fdafc09bf527582a24b0
Sebeom Lee <moses97@gmail.com>
2020-06-11 21:54:20 +0900

    Change deploy setting

--- setup.py
+++ setup.py
@@ -8,12 +8,19 @@ def get_readme():
         readme = readme_file.read()
         return readme
 
+def get_requirements():
+    here = path.dirname(__file__)
+    with open(path.join(here, 'requirements.txt'), encoding='UTF8') as req_file:
+        req = req_file.read()
+        return req
+
 setup(
     name="pyplayscii",
     version="0.0.1",
     author="SeBeom Lee",
     description="Unity style ascii art game engine",
     long_description=get_readme(),
+    install_requires=get_requirements(),
     long_description_content_type="text/markdown",
     url="https://github.com/k2sebeom/pyplayscii",
     packages=find_packages(include=['playscii']),
