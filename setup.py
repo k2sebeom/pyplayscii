12d54ce46cbc8cb13ef4e5785f1edc9df896901d
k2sebeom <moses97@gmail.com>
2020-07-14 01:36:44 +0000

    Bump version: 0.2.6 → 1.0.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_requirements():
 
 setup(
     name="pyplayscii",
-    version="0.2.6",
+    version="1.0.0",
     author="SeBeom Lee",
     description="Object oriented ascii art python game engine",
     long_description=get_readme(),
